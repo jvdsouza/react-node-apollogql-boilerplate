0d8d051189129e57001b23f0c419e9313d2c80d5
Jason D'souza <jvincentdsouza@gmail.com>
2019-12-15 19:24:37 +1100

    removing duplicated code

--- API/server.js
+++ API/server.js
@@ -19,16 +19,12 @@ const pong = require('./controllers/pong');
 
 //----requests-----
 app.get('/', (req, resp) => {
-    resp.sendFile(path.join(__dirname + '/index.html'))
+    resp.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.get('/ping', (req, resp) => {
     pong(req, resp);
 })
-
-app.use(bodyParser.json())
-app.use(helmet())
-app.use(cors())
 
 //---connection listening---
 app.listen(port || 5000, () => {
